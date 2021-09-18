c632da85575624c0d0d8fb98ab31e65faeee2baf
Abdullah Al Akash <aabdullahalakash@gmail.com>
2021-09-18 18:13:00 +0600

    Loaded Single Item Products

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const productContainer = document.getElementById('product-container');
+
 // Load Default Display Data:
 fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline`)
         .then(res => res.json())
@@ -5,7 +7,7 @@ fetch(`https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline`)
 
 // Display Product Data:
 const displayProduct = (products) => {
-        const productContainer = document.getElementById('product-container');
+        productContainer.textContent = '';
         console.log(products[0]);
         products.forEach((product) => {
                 const { brand, name, price, image_link } = product;
@@ -21,7 +23,47 @@ const displayProduct = (products) => {
                                                         <h5 class="card-title">${name}</h5>
                                                 </div>
                                                 <p class="m-0 p-0">Brand Name: ${brand}</p>
-                                                <h3>$${price}</h3>
+                                                <h3 class="product-price">$${price}</h3>
+                                                <button type="button" class="btn btn-danger fw-bold" id="add-cart">Add
+                                                        Cart</button>
+                                        </div>
+                                        <div class="">
+                                                <small class="text-muted">Last updated 3 mins ago</small>
+                                        </div>
+
+                                </div>
+                        </div>
+                `;
+                productContainer.appendChild(productDiv);
+        })
+}
+
+// Loaded Item Products By Menu Click:
+const singleItem = (url) => {
+        // console.log(url)
+        fetch(url)
+                .then(res => res.json())
+                .then(data => displaySingleItem(data))
+}
+
+const displaySingleItem = (products) => {
+        productContainer.textContent = '';
+        console.log(products[0]);
+        products.forEach((product) => {
+                const { brand, name, price, image_link } = product;
+                const productDiv = document.createElement('div');
+                productDiv.innerHTML = `
+                                <div class="col">
+                                <div class="h-100 product-card">
+                                        <div class="product-image">
+                                                <img src="${image_link}" class="img-fluid" alt="...">
+                                        </div>
+                                        <div class="card-body">
+                                                <div class="product-name overflow-hidden">
+                                                        <h5 class="card-title">${name}</h5>
+                                                </div>
+                                                <p class="m-0 p-0">Brand Name: ${brand}</p>
+                                                <h3 class="product-price">$${price ? price : 20.99}</h3>
                                                 <button type="button" class="btn btn-danger fw-bold" id="add-cart">Add
                                                         Cart</button>
                                         </div>
